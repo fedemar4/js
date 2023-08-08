389956c2afe9a18df1d059cadd031916cabaee3c
fede martinez <federicoamodil23@gmail.com>
2023-08-08 18:22:20 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let edad = prompt("ingrese su edad");
+
+if (edad >= 18) {
+    alert ("sos mayor de edad, quedate y hace tu calculo tranquilo") } else{  alert("sos menor de edad, para poder calcular hacelo con un adulto") }  
+
+    const pantalones = ["Jean", "Chino", "Carga"];
+    const remeras = ["Manga corta", "Manga larga", "Sin mangas"];
+    const camperas = ["Cuero", "Denim", "Bomber"];
+    
+
+    function obtenerSeleccion(opciones, categoria) {
+      console.log(`Elige una opción de ${categoria}:`);
+      for (let i = 0; i < opciones.length; i++) {
+        console.log(`${i + 1}. ${opciones[i]}`);
+      }
+      const eleccion = prompt(`Ingresa el número de la opción de ${categoria}:`);
+      return opciones[parseInt(eleccion) - 1];
+    }
+    
+  
+    const pantalonElegido = obtenerSeleccion(pantalones, "pantalones");
+    const remeraElegida = obtenerSeleccion(remeras, "remeras");
+    const camperaElegida = obtenerSeleccion(camperas, "camperas");
+    
+
+    console.log(`¡Elegiste la combinación: ${pantalonElegido} + ${remeraElegida} + ${camperaElegida}!`);
